e1c2acea8b083370886694a14586e34a41006091
ammarzeyad <ammarramadan16@yahoo.com>
2021-08-01 18:50:32 +0300

    Done lab1D

---
+++ app.js
@@ -0,0 +1,18 @@
+let name = prompt("Heyy What Is Your name ?");
+
+alert("Heey, wlecome To My Modest Website " + name);
+
+let age = Number(prompt("How Old are You ?"));
+
+  console.log(age);
+ 
+
+ let rate = prompt("How you rate my site");
+
+ console.log(rate);
+
+ let y = prompt("Are you happy or Sad ? ")
+ console.log(y);
+
+ alert(" You Name is " +name + " \nAnd you age is " + age + " \nThank you for rating the site " + rate + " \nAlso You are " + y );
+ 
